ac1ad8f0a416d1fc764a8cbf835c1b44ccb1f9a1
brs_tor <boristor22@gmail.com>
2024-04-03 01:58:20 -0400

    Added an Interceptor to Serialize DTOs

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,6 +4,8 @@ import { CreatePostDto } from './dtos/create-post.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { PostDto } from './dtos/post.dto';
 
 @Controller('posts')
 @UseGuards(AuthGuard)
@@ -11,6 +13,7 @@ export class PostsController {
     constructor(private postsService: PostsService) { }
 
     @Post()
+    @Serialize(PostDto)
     createPost(@Body() body: CreatePostDto, @CurrentUser() user: User) {
         /* TODO - Relationship with User */
         return this.postsService.create(body, user);
