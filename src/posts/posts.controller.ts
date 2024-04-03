4bfe3b4e9df8be689ee88f328cae29a5afe7153e
brs_tor <boristor22@gmail.com>
2024-04-03 05:02:48 -0400

    Added more endpoints for PostsController

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -8,6 +8,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { PostDto } from './dtos/post.dto';
 import { PatchPostStatusDto } from './dtos/patch-post-status';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { PostDetailedDto } from './dtos/post-detailed.dto';
 
 @Controller('posts')
 @UseGuards(AuthGuard)
@@ -21,9 +22,64 @@ export class PostsController {
         return this.postsService.create(body, user);
     }
 
+    @Get('id/:id')
+    @Serialize(PostDetailedDto)
+    async findPostDetailedById(@Param('id') id: string) {
+        const post = await this.postsService.findOneById(parseInt(id));
+        if (!post) {
+            throw new NotFoundException('post not found')
+        }
+        return post
+    }
+
+    @Get()
+    @Serialize(PostDetailedDto)
+    async find() {
+        const post = await this.postsService.find();
+        if (post.length === 0) {
+            throw new NotFoundException("There's no posts")
+        }
+        return post
+    }
+
+    @Get('published')
+    @Serialize(PostDetailedDto)
+    async findPublishedPostsDetailed() {
+        const post = await this.postsService.findPublished();
+        if (post.length === 0) {
+            throw new NotFoundException("There's no post published")
+        }
+        return post
+    }
+
+    @Get('drafted')
+    @Serialize(PostDetailedDto)
+    @UseGuards(AdminGuard)
+    async findDrafteddPostsDetailed() {
+        const post = await this.postsService.findDrafted();
+        if (post.length === 0) {
+            throw new NotFoundException("There's no post published")
+        }
+        return post
+    }
+
     @Patch('/:id')
     @UseGuards(AdminGuard)
     chageStatus(@Param('id') id: string, @Body() body: PatchPostStatusDto) {
         return this.postsService.patchStatus(parseInt(id), body.status);
+    }
+
+    @Delete('/:id')
+    @Serialize(PostDetailedDto)
+    async removePost(@Param('id') id: string, @CurrentUser() user: User) {
+        console.log(user.username)
+        const post = await this.postsService.findOneById(parseInt(id));
+        if (!post) {
+            throw new NotFoundException('post not found');
+        }
+        if (post.author.username !== user.username) {
+            throw new BadRequestException("post doesn't belong to that user");
+        }
+        return this.postsService.delete(post);
     }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,16 +1,17 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, NotFoundException, UseGuards } from '@nestjs/common';
+import { Not, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from './post.entity';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { User } from 'src/users/user.entity';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Injectable()
 export class PostsService {
 
-    constructor(@InjectRepository(Post) private repo: Repository<Post>) {}
+    constructor(@InjectRepository(Post) private repo: Repository<Post>) { }
 
-    create(postDto: CreatePostDto, user: User){
+    create(postDto: CreatePostDto, user: User) {
         /* TODO - Relationship with User */
         const post = this.repo.create(postDto);
         post.author = user;
@@ -25,6 +26,46 @@ export class PostsService {
         }
 
         post.status = status;
+        return this.repo.save(post)
+    }
+
+    async findOneById(id: number) {
+        const post = await this.repo.findOne({
+            relations: ['author'],
+            where: { id }
+        })
+        return post
+    }
+
+    async find() {
+        const post = await this.repo.find({
+            relations: ['author'],
+            where: {status: Not('deleted')}
+        })
+        return post
+    }
+
+    async findPublished() {
+        const post = await this.repo.find({
+            relations: ['author'],
+            where: { status: 'published' },
+            order: { created_at: "DESC" }
+        })
+        return post
+    }
+
+    @UseGuards(AdminGuard)
+    async findDrafted() {
+        const post = await this.repo.find({
+            relations: ['author'],
+            where: { status: 'drafted' },
+            order: { created_at: "ASC" }
+        })
+        return post
+    }
+
+    delete(post: Post) {
+        post.status = 'deleted'
         return this.repo.save(post)
     }
 }
