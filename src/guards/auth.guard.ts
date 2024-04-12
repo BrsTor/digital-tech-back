12d83ad93a48c9f6f0bf4ba6c81c0ed1a3493e90
brs_tor <boristor22@gmail.com>
2024-04-12 14:06:20 -0400

    Changed the return datatype to bool on Auth Guard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -5,7 +5,12 @@ export class AuthGuard implements CanActivate {
         //Retieve the request from the context
         const request = context.switchToHttp().getRequest();
 
+        //If the userId doesn't exist in the session of the request it means it cannot Activate
+        if (!request.session.userId) {
+            return false
+        }
+
         //If the userId exist in the session of the request it means it canActivate and give access to the Guard
-        return request.session.userId;
+        return true;
     }
 }
